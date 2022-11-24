091b3c57a8d8a575e3a048b8b5ab285a6afdef97
juan usuga <jusugav@unal.edu.co>
2022-11-24 13:13:06 -0500

    fix build

--- src/components/GeoMap/GeoMap.jsx
+++ src/components/GeoMap/GeoMap.jsx
@@ -1,10 +1,17 @@
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import Map, { Source, Layer } from 'react-map-gl';
 import PropTypes from 'prop-types';
 import React, { useState, useCallback } from 'react';
+import mapboxgl from 'mapbox-gl';
+
 
 import './style.css';
 
 import { dataLayer } from './map-style';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoianVzdWdhdiIsImEiOiJjbGFxN28wc3YxZzl0M3JtaTUyb21wOGFtIn0.j5-unL3t1TDt11kCSI1_WA'; // Set your mapbox token here
 
@@ -14,8 +21,12 @@ const GeoMap = ({ data }) => {
   const onHover = useCallback(event => {
     const {
       features,
-      point: { x, y }
+      point,
     } = event;
+
+    const x = point?.x;
+    const y = point?.y;
+
     const hoveredFeature = features && features[0];
 
     // prettier-ignore
